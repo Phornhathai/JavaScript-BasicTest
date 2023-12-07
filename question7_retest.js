027e076ce03b7581ca25c555afbcc8d6e220f42c
Phornhathai <phornhathai.aum@gmail.com>
2023-12-07 17:34:01 +0700

    update question7

--- question7_retest.js
+++ question7_retest.js
@@ -1,8 +1,9 @@
 //sortOdd ([5,28,3,10,7,1]) -> [1,28,3,10,5,7]
 
-const test1 = [5,28,3,10,7,1] 
+const test1 = [5,3,2,10,9,1] 
 
 // step 1 stored index and value of even array
+// step 2 sort even array Less to More
 const sortedOdd = (arr) =>{
     const storeEventIndex = arr.map((value, index) =>({
         number : value,
@@ -10,13 +11,15 @@ const sortedOdd = (arr) =>{
     })).filter((value) => value.number % 2 === 0)
 
     const oddArr = arr.filter((value) => value % 2 !== 0).sort((a , b) => a - b)
+    // step 3 use splice to insert odd value 
+    storeEventIndex.forEach(value => {
+        oddArr.splice(value.oldIndex, 0 , value.number)
+    });
 
-
+    return oddArr
 }
 
 
-// step 2 sort even array Less to More
 
-// step 3 use splice to insert odd value 
 const result = sortedOdd(test1)
 console.log(result);
