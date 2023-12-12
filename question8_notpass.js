2bf0884c11f33089c673fba5040cee9c1122365f
Aummii Desu <74613199+Phornhathai@users.noreply.github.com>
2023-12-12 13:45:01 +0700

    Update question8_notpass.js

--- question8_notpass.js
+++ question8_notpass.js
@@ -1,14 +1,14 @@
-// function sumNumTarget(arr,target){
-//     for (let i = 0; i < arr.length ; i++) {
-//         for (let j = 0; j < arr.length ; j++) {
-//             if(arr[i] + arr[j] == target){
-//                 console.log(i,j);
-//                 i = arr.length           
-//             }
+function sumNumTarget(arr,target){
+    for (let i = 0; i < arr.length ; i++) {
+        for (let j = 0; j < arr.length ; j++) {
+            if(arr[i] + arr[j] == target){
+                console.log(i,j);
+                i = arr.length           
+            }
             
-//         }
-//     }
+        }
+    }
 
-// }
+}
 
-// sumNumTarget([4,7,2] , 6)
+sumNumTarget([4,7,2] , 6)
